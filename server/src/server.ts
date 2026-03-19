927c9f362d6a34c1be26c676ac1ba251d4dd9398
fushigura <hanzo343323@gmail.com>
2026-03-19 14:05:17 +0200

    Upload latest version

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import { schema } from './schema';
-
-const raw = require('express-graphql');
-const graphqlHTTP: any = (raw && raw.graphqlHTTP) ? raw.graphqlHTTP : raw;
+import { createHandler } from 'graphql-http/lib/use/express';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+app.all('/graphql', createHandler({ schema }));
 
 app.listen(4000, () => {
   console.log('Server running on http://localhost:4000/graphql');
